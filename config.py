edfe78d71de4cd6fbcde561fa70c42ee8a3cd2cd
TEPEN-CHAN <tanpasauce@gmaail.com>
2026-06-03 21:53:39 +0700

    final ntah keberapa

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
-MYSQL_HOST = "localhost"
-MYSQL_USER = "root"
-MYSQL_PASSWORD = ""
-MYSQL_DB = "sistem_knn_jurusan"
+import os
 
-SECRET_KEY = "secret123"
+MYSQL_HOST = os.getenv('MYSQL_HOST')
+MYSQL_USER = os.getenv('MYSQL_USER')
+MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+MYSQL_DB = os.getenv('MYSQL_DB')
+MYSQL_PORT = int(os.getenv('MYSQL_PORT', 3306))
+SECRET_KEY = os.getenv('SECRET_KEY')
